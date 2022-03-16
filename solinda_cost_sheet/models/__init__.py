266078aa67932cd0143716fead97bc1818bcd4f0
Siwi Wiyono <wiyonosiwi@gmail.com>
2022-03-16 13:23:34 +0700

    initial commit

---
+++ solinda_cost_sheet/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import crm
+from . import approval
+from . import cost_sheet
+from . import purchase_requisition
+from . import project
+from . import sale
+
+# from . import sale
